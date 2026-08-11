a78facb30e2bab5ba04b531339efb323c1409b05
Vedant Tonde <148040064+Dreammm7@users.noreply.github.com>
2026-08-11 17:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (7.20%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int MP = 0;
+        int bestbuy = prices[0];
+
+        for(int i=1; i < prices.size(); i++){
+            if(prices[i] > bestbuy){
+                MP = max(MP, prices[i]-bestbuy);
+            }
+            bestbuy = min(bestbuy, prices[i]);
+        }
+        return MP;
+    }
+};
