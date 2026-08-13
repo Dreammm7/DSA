8b68efb81bd2abd3391e86a0be6c6e5c5c378f55
Vedant Tonde <148040064+Dreammm7@users.noreply.github.com>
2026-08-13 20:04:00 +0530

    Time: 2 ms (43.86%) | Memory: 11.8 MB (18.22%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+       vector<string> ans;
+        for(int i = 1; i <= n; i++){
+            string currstr = "";
+
+            if (i % 3 == 0) currstr += "Fizz";
+            if (i % 5 == 0) currstr += "Buzz";
+    
+            if (currstr == "") {
+                currstr = to_string(i);
+            }
+            ans.push_back(currstr);
+        }
+        return ans;
+    }
+};
